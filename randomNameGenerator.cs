14841874681b307991884a8252012fea45b4e17c
SamLewis98 <108067953+SamLewis98@users.noreply.github.com>
2022-11-27 22:21:47 +0000

    Add files via upload

---
+++ randomNameGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNameGenerator
+{
+    public class NamesGenerator
+    {
+        
+        private static string[] teamMember = { "Sam", "Nick", "Chris", "Josh", "Sarah","Jarad", "Mary", "John", "Rachel","Sophie", "Karl", "Jo" };
+
+        public static string GenerateName()
+        {
+            var rand = new Random();
+            string randomTeamMember = teamMember[rand.Next(teamMember.Length)];
+
+            return $"{randomTeamMember}";
+        }
+    }
+}
